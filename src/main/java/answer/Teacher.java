f2bd7b9bc702080468a15128766bed2fe1e2947f
Manda Tiavina <solontianamandanirina@gmail.com>
2025-11-17 22:26:48 +0400

    feat: add teacher value

--- src/main/java/answer/Teacher.java
+++ src/main/java/answer/Teacher.java
@@ -1,4 +1,17 @@
 package answer;
 
-public class Teacher {
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+public class Teacher extends Person{
+    private TeacherSpeciality speciality;
+
+    public Teacher(int id, String lastName, String firstName, LocalDate birthDate, String email, String phoneNumber, TeacherSpeciality specialty) {
+        super(id, lastName, firstName, birthDate, email, phoneNumber);
+    this.speciality = specialty;
+    }
 }
---
+++ src/main/java/answer/TeacherSpeciality.java
@@ -0,0 +1,5 @@
+package answer;
+
+public enum TeacherSpeciality {
+    Front_end, Back_end, Full_stack, Devops
+}
