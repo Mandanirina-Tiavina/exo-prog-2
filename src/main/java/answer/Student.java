47661a27399430159b5ae7f70467d090826a2162
Manda Tiavina <solontianamandanirina@gmail.com>
2025-11-17 22:05:55 +0400

    feat: implement student values

--- src/main/java/answer/Student.java
+++ src/main/java/answer/Student.java
@@ -1,4 +1,17 @@
 package answer;
 
-public class Student {
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Student extends Person {
+    private String group;
+    private Tutor tutor;
+    public Student(int id, String firstName, String lastName, LocalDate birthDate, String email,String phoneNumber, String group, Tutor tutor) {
+        super(id, lastName, firstName, birthDate, email, phoneNumber);
+        this.group = group;
+        this.tutor = tutor;
+    }
 }
